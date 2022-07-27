fa439a708c9c9f9ca39dc1ac1e0c9f1e383367bd
Cefas Sathler <cefasheli@gmail.com>
2022-07-27 20:01:37 -0300

    27/07

---
+++ Utilities/healerTabajara.js
@@ -0,0 +1,92 @@
+const targetsAj = Array.from(game.user.targets);
+
+//MINOR HEALING
+
+
+function minorHealing(energy) {
+  if ((energy > 3)) {
+    ui.notifications.error("The value need to be equal or lower than 3");
+    dialogEditor.render(true);
+        
+ 
+//  energy = Object.keys(energy).length === 0 {
+//       ui.notifications.error("Choose a value between 1-3");
+      
+      
+
+    } else {
+      
+      
+      GURPS.executeOTF(`/r [/hp +${energy}  @target]`)
+      GURPS.executeOTF(`/r [/fp -${energy}]`)
+    }
+  }
+
+
+  //MAJOR HEALING
+
+
+function majorHealing(energy) {
+  if ((canvas.tokens.controlled[0] === 0 & Array.from(game.user.targets)[0] === 0)) {
+    ui.notifications.error("You need some energy to cast this spell!");
+    dialogEditor.render(true);
+
+  } else {
+    let realHealing = energy * 2
+    targets.forEach((target) => {
+    GURPS.executeOTF(`/r [/hp +${realHealing}  @target]`)
+    GURPS.executeOTF(`/r [/fp -${energy}]`);
+    }
+    )};
+  
+
+}
+
+//GREATER HEALING
+
+function greaterHealing(energy) {
+
+  GURPS.executeOTF(`/r [/hp reset  @target]`)
+  GURPS.executeOTF(`/r [/fp -20]`)
+}
+
+let dialogContent =
+` 
+style
+    <form>
+    <div class="form-group">
+      <label>Fatigue Points:</label>
+      <input id="energy" name="energy" type="number" value="1" min="1" max="20">
+      </div>    
+`
+  ;
+
+//let applyChanges = false
+let dialogButtons = {
+ 
+
+  minorHealing: {
+
+    label: `Minor Healing`,
+    callback: async (html) => minorHealing(html.find('#energy').val())
+  },
+  majorHealing: {
+
+    label: `Major Healing`,
+    callback: async (html) => majorHealing(html.find('#energy').val())
+  },
+  greaterHealing: {
+
+    label: `Greater Healing`,
+    callback: async (html) => greaterHealing(html.find('#energy').val())
+  }
+}
+
+let dialogEditor = new Dialog({
+  title: `Instant Healings`,
+  content: dialogContent,
+  buttons: dialogButtons,
+  default: "yes",
+})
+
+dialogEditor.render(true);
---
+++ nothingHereWorks/tick copy.js
@@ -0,0 +1,84 @@
+/* AE Builder
+docs: https://gitlab.com/peginc/swade/-/wikis/active-effects#attribute-keys
+*/
+
+const version = 'v1.7';
+const icon = "icons/magic/symbols/rune-sigil-green.webp";
+
+if (canvas.tokens.controlled[0] === undefined && Array.from(game.user.targets)[0] === undefined) {
+  ui.notifications.error("Please, select or target a token."); // No Token is Selected
+} else {
+  main();
+}
+
+function main() {
+  let tokenD = canvas.tokens.controlled[0];
+
+
+
+  let dialog_content = `
+
+  <form>
+      <div class="form-group">
+        <label>Fatigue Points:</label>
+        <input id="energy" name="energy" type="text" value="How many Points? ">
+      </div>    
+  <div class="form-group">
+  
+  
+  <td style="text-align: center;"><input type="checkbox" id="minortick" style="margin-left:15px; margin-right:6px" value=yes<label>Minor Healing</label></td>
+  
+  <td style="text-align: center;"><input type="checkbox" id="majortick" style="margin-left:15px; margin-right:6px" value=yes<label>Major Healing</label></td> 
+  
+  <td style="text-align: center;"><input type="checkbox" id="greatertick" style="margin-left:15px; margin-right:6px" value=yes<label>Greater Healing</label></td>
+   
+`;
+
+  let applyChanges = false;
+  let dialogButtons = {
+    yes: {
+      icon: "<i class='fas fa-check'></i>",
+      label: `Cast Spells`,
+      callback: async (html) => applySpell(html.find('#energy').val())
+    },
+    no: {
+      icon: "<i class='fas fa-times'></i>",
+      label: `Cancel Spell`
+    }
+  }
+
+  // título   
+  
+}
+
+//async function applySpell(html) {
+//  let energy = html.find('[name="energy"]')[0].value;
+//}
+
+
+const minH = document.querySelector('#minortick');
+console.log(minH.checked);
+const majH = document.querySelector('#majortick');
+console.log(majH.checked);
+const gH = document.querySelector('#greatertick');
+console.log(gH.checked);
+
+
+//minor healing
+
+if (console.log(minH) === true) {
+  ui.notifications.error("Escolha uma magia");
+
+  } else {
+
+function applySpell(energy) {
+    
+    GURPS.executeOTF(`/r [/hp +${realHealing}  @target]]`)
+    GURPS.executeOTF(`/r [/fp -${energy}]`)
+  }
+}
+// function applySpell(energy) {
+//   let realHealing = energy *2
+//    GURPS.executeOTF(`/r [/hp +${realHealing}  @target]]`)
+//    GURPS.executeOTF(`/r [/fp -${energy}]`)
+//   }
